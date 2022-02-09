01323fb0d6442fc07463552ab453563da95c8f59
Jakub Zomerfeld <jakub.zomerfeld@enginiety.com>
2022-02-09 14:16:08 +0100

    fix: disable checkout btn appropriately

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,6 +56,10 @@ export class AppComponent implements OnInit {
     })
   }
 
+  isCheckoutDisabled() {
+    return !this.rentalCalculator.rentalDays || this.rentalCalculator.rentalPrice == 0
+  }
+
   getDailyPriceWithCurrency(vehicleClass: any) {
     return this.pricingService.getByClass(vehicleClass)
   }
