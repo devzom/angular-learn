4921cba8806ecd1984d1e1a58e2d7a8d823b89ba
Jakub Zomerfeld <jakub.zomerfeld@enginiety.com>
2022-02-09 14:26:28 +0100

    style: extend <base-button/> class and fullp-width option

--- src/app/components/base/button/button.component.ts
+++ src/app/components/base/button/button.component.ts
@@ -3,7 +3,9 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'base-button',
   template: `
-    <button [disabled]="disabled" disabled class="btn btn-sm btn-dark m-2">
+    <button [disabled]="disabled"
+            [ngClass]="{'w-100 mx-auto': full}"
+            disabled class="btn btn-sm  btn-dark m-2">
       <ng-content></ng-content>
     </button>
   `,
@@ -11,4 +13,5 @@ import {Component, Input} from '@angular/core';
 })
 export class ButtonComponent {
   @Input() disabled: boolean = true;
+  @Input() full: boolean = false;
 }
