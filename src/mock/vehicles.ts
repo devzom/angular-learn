dca3b83c0c9ad5133d79807bfbaa9b622af29d21
Jakub Zomerfeld <jakub.zomerfeld@enginiety.com>
2022-02-09 12:15:12 +0100

    feat: add vehicle mock data

---
+++ src/mock/vehicles.ts
@@ -0,0 +1,25 @@
+export default [{
+  id: 4123123,
+  make: 'Volvo',
+  model: "V60",
+  year: 2021,
+  class: 'A',
+  imageSrc: '/assets/images/volvo-v60.jpeg'
+},
+  {
+    id: 51231235,
+    make: 'Volvo',
+    model: "XC60",
+    year: 2019,
+    class: 'B',
+    imageSrc: '/assets/images/volvo-xc60.jpeg'
+
+  },
+  {
+    id: 14145123,
+    make: 'Volvo',
+    model: "V40",
+    year: 2017,
+    class: 'C',
+    imageSrc: '/assets/images/volvo-v40.jpeg'
+  }]
